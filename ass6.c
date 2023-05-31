5d61572433a38de10be5abbe582d2da853c51b10
Aditya Bodewar <98313615+madbunny253@users.noreply.github.com>
2023-05-31 17:24:46 +0530

    Update ass6.c

--- ass6.c
+++ ass6.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 void todelay(void)
 {
-T0CON=0x08;
-TMR0H=0x35;
-TMR0L=0x00;
-T0CONbits.TMR0ON=1;
-while(INTCONbits.TMR0IF==0);
-T0CONbits.TMR0ON=0;
-INTCONbits.TMR0IF=0;
+    T0CON=0x08;
+    TMR0H=0x35;
+    TMR0L=0x00;
+    T0CONbits.TMR0ON=1;
+    while(INTCONbits.TMR0IF==0);
+    T0CONbits.TMR0ON=0;
+    INTCONbits.TMR0IF=0;
 }
 void main(void) {
 TRISD=0;
