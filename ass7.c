c8515c1bd8fc11d91e03ffe5f286388d86ed1582
Aditya Bodewar <98313615+madbunny253@users.noreply.github.com>
2023-06-01 07:39:28 +0530

    Update ass7.c

--- ass7.c
+++ ass7.c
@@ -1,96 +1,37 @@
-/*
- * File:   buzzer.c
- * Author: ADITYA
- *
- * Created on 31 May, 2023, 5:33 PM
- */
-
-
 #include <xc.h>
-#include <pic18f4550.h>
-#define buzzer LATAbits.LATA5
+#include<pic18f4550.h>
+#define Buzzer LATAbits.LATA5 //define buzzer pin
 
-void delay()
+unsigned int count=0;
+
+void Timer1ISR()
 {
-    int i,j;
-    
-    for(i=0;i<200;i++)
-    {
-        for(j=0;j<500;j++)
-        {
-            
-        }
-    }
+if(TMR1IF==1)
+{
+TMR1L=0x20;
+TMR1H=0xD1;
+count++;
+if(count>=1000)
+{ 
+Buzzer=~Buzzer; //Toggle buzzer pin
+count=0;
 }
-
-void timer1ISR()
-{
-    if(TMR1IF==1)
-    {
-        TMR1L=0x20;
-        TMR1H=0xD1;
-        
-        buzzer=~buzzer;
-        delay();
-        
-        TMR1IF=0;
-    }
+TMR1IF=0;
 }
-
-void main(void) 
-{
-    TRISAbits.TRISA5=0;
-    T1CON=0x80;
-    
-    GIE=1; 
-    PEIE=1;
-    TMR1IE=1;
-    TMR1IF=0;
-    
-    TMR1L=0x20;
-    TMR1H=0xD1;
-    TMR1ON=1;
-    
-    while(1);
 }
-
-#include <xc.h>
-#include <pic18f4550.h>
-#define buzzer LATAbits.LATA5
-
-int count;
-void timer1ISR()
-{
-    if(TMR1IF==1)
-    {
-        TMR1L=0x20;
-        TMR1H=0xD1;
-        count++
-        if(count>1000)
-        {
-            buzzer=~buzzer;
-            count=0;
-        }
-        
-        
-        
-        TMR1IF=0;
-    }
-}
-
-void main(void) 
-{
-    TRISAbits.TRISA5=0;
-    T1CON=0x80;
-    
-    GIE=1; 
-    PEIE=1;
-    TMR1IE=1;
-    TMR1IF=0;
-    
-    TMR1L=0x20;
-    TMR1H=0xD1;
-    TMR1ON=1;
-    
-    while(1);
+void main(void)
+ {
+TRISB=0; //set as output
+TRISAbits.TRISA5=0; //set buzzer pin as RA5 as output
+GIE=1;
+PEIE=1;
+TMR1IE=1;
+TMR1IF=0;
+/*Enable 16 bit TMR1 register,no pre-scaler,internal clock,timer OFF*/
+T1CON=0x80;	
+TMR1L=0x20;
+TMR1H=0xD1;
+TMR1ON=1;
+while(1);
+return;
 }
